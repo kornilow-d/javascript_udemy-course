eb72a70414b34c0def192cd7bcbf20f0f19a3c71
Vadim Kornilov <kornilow.home@gmail.com>
2020-05-28 14:43:09 +0700

    Use Class to Work

--- js/script.js
+++ js/script.js
@@ -102,9 +102,9 @@ window.addEventListener('DOMContentLoaded', function () {
 			document.body.style.overflow = '';
 		},
 		init() {
-			let modalTimerId = setTimeout(() => {
-				modal.open();
-			}, 5000);
+			// let modalTimerId = setTimeout(() => {
+			// 	modal.open();
+			// }, 5000);
 			modal.btnsOpen.forEach(btn => {
 				btn.addEventListener('click', () => {
 					modal.open();
@@ -136,34 +136,71 @@ window.addEventListener('DOMContentLoaded', function () {
 	// const ivan = new User('Ivan', 28);
 	// const alex = new User('Alex', 20);
 
-	class Rectangle {
-		constructor(height, width) {
-			this.height = height;
-			this.width = width;
-		}
+	// class Rectangle {
+	// 	constructor(height, width) {
+	// 		this.height = height;
+	// 		this.width = width;
+	// 	}
 
-		calcArea() {
-			return this.height * this.width;
-		}
-	}
+	// 	calcArea() {
+	// 		return this.height * this.width;
+	// 	}
+	// }
 
-	// Наследуем свойства от родителя
-	class ColoredRectangleWithText extends Rectangle {
-		constructor(height, width, text, bgColor) {
-			super(height, width); // Вызываем тоже самое, что было у родителя + всегда на первом месте
-			this.text = text;
-			this.bgColor = bgColor;
-		}
+	// // Наследуем свойства от родителя
+	// class ColoredRectangleWithText extends Rectangle {
+	// 	constructor(height, width, text, bgColor) {
+	// 		super(height, width); // Вызываем тоже самое, что было у родителя + всегда на первом месте
+	// 		this.text = text;
+	// 		this.bgColor = bgColor;
+	// 	}
 
-		showMyProps() {
-			console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
-		}
-	}
+	// 	showMyProps() {
+	// 		console.log(`Текст: ${this.text}, цвет: ${this.bgColor}`);
+	// 	}
+	// }
 
-	const div = new ColoredRectangleWithText(25, 10, 'Hello', 'red');
+	// const div = new ColoredRectangleWithText(25, 10, 'Hello', 'red');
 
-	div.showMyProps();
-	console.log(div.calcArea());
+	// div.showMyProps();
+	// console.log(div.calcArea());
 
 	// Первый принцип ООП - Абстракция - когда мы разделяем концепцию (шаблона), от её экземпляра
+
+	// Карточки
+	class MenuCard {
+		constructor(src, alt, title, descr, price, parentSelector) {
+			this.src = src;
+			this.alt = alt;
+			this.title = title;
+			this.discr = descr;
+			this.price = price;
+			this.parent = document.querySelector(parentSelector);
+			this.transfer = 27;
+			this.changeToUAH();
+		}
+
+		changeToUAH() {
+			this.price = this.price * this.transfer;
+		}
+
+		render() {
+			const element = document.createElement('div');
+			element.innerHTML = `
+				<div class="menu__item">
+					<img src="${this.src}" alt="${this.alt}" />
+					<h3 class="menu__item-subtitle">${this.title}</h3>
+					<div class="menu__item-descr">${this.discr}</div>
+					<div class="menu__item-divider"></div>
+					<div class="menu__item-price">
+						<div class="menu__item-cost">Цена:</div>
+						<div class="menu__item-total"><span>${this.price}</span> грн/день</div>
+					</div>
+				</div>
+			`;
+			this.parent.append(element);
+		}
+	}
+
+	new MenuCard('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" -', 9, '.menu .container').render();
 });
