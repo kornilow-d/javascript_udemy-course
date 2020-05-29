e116978ad69b2269c1080b6088894408fe7aee56
Vadim Kornilov <kornilow.home@gmail.com>
2020-05-29 09:29:02 +0700

    Add POST request

--- js/script.js
+++ js/script.js
@@ -168,25 +168,78 @@ window.addEventListener('DOMContentLoaded', function () {
 
 	new MenuCard('img/tabs/vegy.jpg', 'vegy', 'Меню "Фитнес"', 'Меню "Фитнес" -', 9, '.menu .container').render();
 
-	const inputRub = document.querySelector('#rub');
-	const inputUsd = document.querySelector('#usd');
+	// const inputRub = document.querySelector('#rub');
+	// const inputUsd = document.querySelector('#usd');
 
-	inputRub.addEventListener('input', () => {
-		const request = new XMLHttpRequest();
-		request.open('GET', 'js/current.json');
-		request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
-		request.send();
+	// inputRub.addEventListener('input', () => {
+	// 	const request = new XMLHttpRequest();
+	// 	request.open('GET', 'js/current.json');
+	// 	request.setRequestHeader('Content-type', 'application/json; charset=utf-8');
+	// 	request.send();
 
-		request.addEventListener('load', () => {
-			if (request.status === 200) {
-				console.log(request.response);
-				const data = JSON.parse(request.response);
-				inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
-			} else {
-				inputUsd.value = 'Что-то пошло не так';
-			}
-		});
+	// 	request.addEventListener('load', () => {
+	// 		if (request.status === 200) {
+	// 			console.log(request.response);
+	// 			const data = JSON.parse(request.response);
+	// 			inputUsd.value = (+inputRub.value / data.current.usd).toFixed(2);
+	// 		} else {
+	// 			inputUsd.value = 'Что-то пошло не так';
+	// 		}
+	// 	});
 
-		// status / statusText / response / readyState
+	// 	// status / statusText / response / readyState
+	// });
+
+	// Forms
+
+	const forms = document.querySelectorAll('form');
+
+	const message = {
+		loading: 'Загрузка',
+		success: 'Успешно',
+		failure: 'Ошибка',
+	};
+
+	forms.forEach(item => {
+		postData(item);
 	});
+
+	function postData(form) {
+		form.addEventListener('submit', event => {
+			event.preventDefault();
+
+			const statusMessage = document.createElement('div');
+			statusMessage.classList.add('status');
+			statusMessage.textContent = message.loading;
+			form.append(statusMessage);
+
+			const request = new XMLHttpRequest();
+			request.open('POST', 'server.php');
+
+			request.setRequestHeader('Content-type', 'application/json');
+			const formData = new FormData(form);
+
+			const object = {};
+			formData.forEach(function (value, key) {
+				object[key] = value;
+			});
+
+			const json = JSON.stringify(object);
+
+			request.send(json);
+
+			request.addEventListener('load', () => {
+				if (request.status === 200) {
+					console.log(request.response);
+					statusMessage.textContent = message.success;
+					form.reset();
+					setTimeout(() => {
+						statusMessage.remove();
+					}, 2000);
+				} else {
+					statusMessage.textContent = message.failure;
+				}
+			});
+		});
+	}
 });
